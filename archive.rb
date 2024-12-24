e40fbb44ddc108feecdd6e7ad15bf9f4834ebd96
Simon Worthington <simon@simonwo.net>
2024-12-24 07:50:31 +0000

    Account for URLs with slugs and private reblogs.

--- archive.rb
+++ archive.rb
@@ -36,23 +36,30 @@ TUMBLR.posts(:ferronickel, tag: 'looking glasses', notes_info: true).each do |po
 
   reblogs.each do |reblog|
     fetched_post = TumblrLite::post(reblog.blog_name, reblog.post_id)
-    reblog.private = fetched_post.nil?
-    next if fetched_post.nil?
+    if fetched_post.nil?
+      reblog.private = true
+      reblog.url = "https://www.tumblr.com/blog/view/#{reblog.blog_name}/#{reblog.post_id}"
+    else
+      reblog.private = false
+      reblog.tags = fetched_post.tags
+      reblog.url = fetched_post.url
+      reblog.reblogged_from = fetched_post.send(:"reblogged-from-url").split("/").select {|path| path =~ /\d+/ }.first
 
-    # If the reblog has added text, we might not have all of it, so we 'd better go get it.
-    if !fetched_post.nil? && !reblog.added_text.nil?
-      full_post = TUMBLR.post(reblog.blog_name, reblog.post_id)
-      reblog.added_text = full_post.reblog.comment
+      # If the reblog has added text, we might not have all of it, so we 'd better go get it.
+      if !fetched_post.nil? && !reblog.added_text.nil?
+        full_post = TUMBLR.post(reblog.blog_name, reblog.post_id)
+        reblog.added_text = full_post.reblog.comment
+      end
     end
 
-    reblog.tags = fetched_post.tags
-
-    # We could compare URLs directly here instead of extracting the ID, which
-    # would probably be safer. But we don't know the URL from the note
-    # structure, so we'd have to look up all the posts first for their URLs, and
-    # then post-process the links between them.
-    response_to = fetched_post.send(:"reblogged-from-url").split("/").last
-    reply_to = notes.filter {|n| n.post_id == response_to }.first
+    reply_to = if reblog.reblogged_from.nil?
+      # If the reblog is private, we have no way of knowing who it was really
+      # reblogging. So as above, we'll assume that it was the most recent reblog
+      # from the named blog.
+      reblogs.filter {|r| r.timestamp <= reblog.timestamp && r.blog_name == reblog.reblog_parent_blog_name }.last
+    else
+      notes.filter {|n| n.post_id == reblog.reblogged_from }.first
+    end
     next if reply_to.nil?
 
     (reply_to.replies ||= []) << reblog
