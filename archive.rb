205934f1ef0677f107bc7bb72637239e3d9a2faf
Simon Worthington <simon@simonwo.net>
2024-12-25 00:57:04 +0000

    Use the `notes` endpoint more strategically
    The various `modes` on the `notes` endpoint return different fields –
    notably, a note that has dded text does not need to have its
    reblogged from ID looked up, as it's included as part of one of the
    modes. So now only do extra lookups if we actually need the info that
    we would get from that lookup.

--- archive.rb
+++ archive.rb
@@ -14,10 +14,25 @@ PAGE_SIZE = 50
 
 TUMBLR = Tumblr.new
 TUMBLR.posts(:ferronickel, tag: ['looking glasses', 'ferrousart'], sort: :asc).each do |post|
+  next unless ARGV.empty? || ARGV.include?(post.id_string)
+
   if post.note_count / PAGE_SIZE > 10
     puts "::warning:: Skipping #{post.id_string} because it has too many notes (#{post.note_count})"
     next
   end
+
+  reblogs = TUMBLR.notes(post.blog_name, post.id, :reblogs_with_tags)
+  conversation = TUMBLR.notes(post.blog_name, post.id, :conversation)
+  replies = conversation.filter {|note| note.type == "reply" }
+
+  conversation.filter {|note| note.type == "reblog" }.each do |note|
+    found_reblog = reblogs.filter {|reblog| reblog.post_id == note.post_id }.first
+    found_reblog.table.update(note.table)
+  end
+
+  post.notes = []
+  post.notes.concat(reblogs)
+  post.notes.concat(conversation.filter {|note| note.type == "reply"})
 
   replies.each_cons(2) do |prev, reply|
     response_to = reply.formatting.filter {|f| f.type == "mention" }.first
@@ -38,30 +53,33 @@ TUMBLR.posts(:ferronickel, tag: ['looking glasses', 'ferrousart'], sort: :asc).e
   end
 
   reblogs.each do |reblog|
-    fetched_post = TumblrLite::post(reblog.blog_name, reblog.post_id)
-    if fetched_post.nil?
-      reblog.private = true
-      reblog.url = "https://www.tumblr.com/blog/view/#{reblog.blog_name}/#{reblog.post_id}"
-    else
-      reblog.private = false
+    reblog.url = "https://www.tumblr.com/blog/view/#{reblog.blog_name}/#{reblog.post_id}"
+
+    if [reblog.tags, reblog.reblog_parent_post_id].any?(&:nil?)
+      fetched_post = TumblrLite::post(reblog.blog_name, reblog.post_id)
+      reblog.private = fetched_post.nil?
+      next if reblog.private
+
       reblog.tags = fetched_post.tags
       reblog.url = fetched_post.url
-      reblog.reblogged_from = fetched_post.send(:"reblogged-from-url").split("/").select {|path| path =~ /\d+/ }.first
+      reblog.reblog_parent_post_id = fetched_post.send(:"reblogged-from-url").split("/").select {|path| path =~ /\d+/ }.first
+    end
 
-      # If the reblog has added text, we might not have all of it, so we 'd better go get it.
-      if !fetched_post.nil? && !reblog.added_text.nil?
-        full_post = TUMBLR.post(reblog.blog_name, reblog.post_id)
+    # If the reblog has added text, we might not have all of it, so we 'd better go get it.
+    if !reblog.private && !reblog.added_text.nil?
+      full_post = TUMBLR.post(reblog.blog_name, reblog.post_id)
+      unless full_post.nil?
         reblog.added_text = full_post.reblog.comment
       end
     end
 
-    reply_to = if reblog.reblogged_from.nil?
+    reply_to = if reblog.reblog_parent_post_id.nil?
       # If the reblog is private, we have no way of knowing who it was really
       # reblogging. So as above, we'll assume that it was the most recent reblog
       # from the named blog.
       reblogs.filter {|r| r.timestamp <= reblog.timestamp && r.blog_name == reblog.reblog_parent_blog_name }.last
     else
-      post.notes.filter {|n| n.post_id == reblog.reblogged_from }.first
+      post.notes.filter {|n| n.post_id == reblog.reblog_parent_post_id }.first
     end
     next if reply_to.nil?
 
--- tumblr.rb
+++ tumblr.rb
@@ -5,21 +5,39 @@ require 'open-uri'
 
 TOKEN_FILE = './.token.yml'
 
+def protect &block
+  attempts = 0
+  begin
+    attempts += 1
+    block.call
+  rescue Net::HTTPError => error
+    case error.response
+    when Net::HTTPTooManyRequests
+      STDERR.puts "\tBack off for #{5 ** attempts}s"
+      sleep (5 ** attempts)
+      retry
+    when Net::HTTPNotFound
+      nil
+    else raise error
+    end
+  end
+end
+
 class Tumblr
   def initialize
     @request_counter = 0
   end
 
   def posts blog, **params
-    get_pages('v2', 'blog', blog, 'posts', **params).flat_map(&:posts)
+    protect { get_pages('v2', 'blog', blog, 'posts', **params).flat_map(&:posts) }
   end
 
   def post blog, id, **params
-    get('v2', 'blog', blog, 'posts', id: id, **params).posts.first
+    protect { get('v2', 'blog', blog, 'posts', id: id, **params).posts.first }
   end
 
   def notes blog, id, mode
-    get_pages('v2', 'blog', blog, 'notes', id: id, mode: mode).flat_map(&:notes)
+    protect { get_pages('v2', 'blog', blog, 'notes', id: id, mode: mode).flat_map(&:notes) }
   end
 
   private
@@ -39,22 +57,25 @@ class Tumblr
     @request_counter += 1
     response = client.request(:get, uri.to_s)
     parsed = JSON::parse response.read_body, object_class: OpenStruct
-    raise "#{parsed.meta.status} #{parsed.meta.msg}: #{parsed.errors.map(&:detail).join('; ')}" if parsed.meta.status > 400
+    if parsed.meta.status >= 400
+      raise Net::HTTPError.new(
+        parsed.errors.map(&:detail).join("\n"),
+        Net::HTTPResponse::CODE_TO_OBJ[parsed.meta.status.to_s].new(response.http_version, parsed.meta.status, parsed.meta.msg),
+      )
+    end
 
     parsed.response
   end
 
   def get_pages *path, **params
-    acc = []
-
-    loop do
-      res = get(*path, **params)
-      acc << res
-      break if res._links.nil? || res._links.next.nil?
-      params = res._links.next.query_params.to_h
+    Enumerator.new do |yielder|
+      loop do
+        res = get(*path, **params)
+        yielder << res
+        break if res._links.nil? || res._links.next.nil?
+        params = res._links.next.query_params.to_h
+      end
     end
-
-    acc
   end
 end
 
@@ -64,24 +85,19 @@ FOOTER = ';'
 class TumblrLite
   class << self
     def post blog, id
-      attempts = 0
-      url = "https://#{blog}.tumblr.com/api/read/json?id=#{id}"
-
-      begin
-        attempts += 1
-        STDOUT.puts "GET #{url}"
-        URI.open(url) do |res|
-          json = res.read[HEADER.size...-(FOOTER.size + 1)]
-          JSON::parse(json, object_class: OpenStruct).posts.first
-        end
-      rescue OpenURI::HTTPError => e
-        case e.to_s
-        when "429 Too Many Requests"
-          STDOUT.puts "\tBack off for #{5 ** attempts}s"
-          sleep (5 ** attempts)
-          retry
-        when "404 Not Found"
-          nil
+      protect do
+        begin
+          url = "https://#{blog}.tumblr.com/api/read/json?id=#{id}"
+          STDOUT.puts "GET #{url}"
+          URI.open(url) do |res|
+            json = res.read[HEADER.size...-(FOOTER.size + 1)]
+            JSON::parse(json, object_class: OpenStruct).posts.first
+          end
+        rescue OpenURI::HTTPError => error
+          raise Net::HTTPError.new(
+            error.message,
+            Net::HTTPResponse::CODE_TO_OBJ[error.message[0...3]].new(nil, error.message[0...3], error.message[5..]),
+          )
         end
       end
     end
